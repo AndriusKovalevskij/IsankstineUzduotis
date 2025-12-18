71331b4d70469ddebf785e20c957933daae1b511
AndriusKovalevskij <andrius.kovalevskij@gmail.com>
2025-12-18 14:05:08 +0200

    Add files via upload

---
+++ header.h
@@ -0,0 +1,19 @@
+#ifndef HEADER_H_INCLUDED
+#define HEADER_H_INCLUDED
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <map>
+#include <string>
+#include <cctype>
+#include <stdexcept>
+
+using namespace std;
+
+string cleanWord(const string& word);
+map<string, int> countWords(const string& fileName);
+void writeWordDataToFile(const map<string, int>& wordCounts, const string& fileName);
+void writeWordDataToTerminal(const map<string, int>& wordCounts);
+
+#endif // HEADER_H_INCLUDED
